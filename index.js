d31ab9950972218b2ad988ca559345f2a6d96d2d
lazorax <riyahere17@gmail.com>
2025-07-28 20:17:43 +0530

    added minmax algorithm

--- index.js
+++ index.js
@@ -1,224 +1,246 @@
-/*
-let btns = document.querySelectorAll(".btn");//array of btns
-let turn = "x";//whose turn?
-let start = false;//
-let compTurn="o";
-let winningPattern = [[0, 1, 2],
-                      [3, 4, 5], 
-                      [6, 7, 8], 
-                      [0, 4, 8],
-                      [2, 4, 6],
-                      [0, 3, 6],
-                      [1, 4, 7],
-                      [2, 5, 8]
-                    ];
-function gameStart(btn) {
-  console.log("turn : " + turn);
-  start = true;
-  btn.innerText = turn;
-  btn.disabled=true;
-  // send the btn to vs player or vs computer 
-}
-
-//min max here
-function vsComputer(){
-
-}
-function playgame(btn) {
-  if (turn === "x") {
-    turn = "o";
-  } else {
-    turn = "x";
-  }
-  btn.innerText = turn;
-  btn.disabled=true;
-  checkWinner();
-}
-
-function checkWinner(){
-    for(pattern of winningPattern){
-      //  console.log(pattern[0],pattern[1],pattern[2]);
-         
-        // console.log(btns[pattern[0]],btns[pattern[1]],btns[pattern[2]]);
-        let btn1 = btns[pattern[0]].innerText;
-        let btn2 = btns[pattern[1]].innerText;
-        let btn3 = btns[pattern[2]].innerText;
-            if(btn1!="" && btn1===btn2 && btn2===btn3){
-              // console.log(turn +" is winner");
-              winner(turn);
-              start = false;
-            }
-      }
-
-}
-function winner(winner){
-  document.querySelector("#result").classList.toggle("hide");
-  document.querySelector("#result h1").innerText=`"${winner}" is winner!`;
- 
-}
- document.querySelector("#restart").addEventListener("click",resetBoard);
- document.querySelector("#start").addEventListener("click",()=>{
-  document.querySelector("#result").classList.toggle("hide");
-  resetBoard();
-});
-
-function resetBoard(){
-  btns.forEach((btn)=>{
-    btn.disabled=false;
-    btn.innerText="";
-    turn="x";
-    
-  })
- }
-btns.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    if (!start) {
-      gameStart(btn);
-      console.log("start game called ");
-    } else {
-      playgame(btn);
-      // console.log("play game called ");
-    }
-  });
-});
-*/
-
-////////////////////////////////////////////////////////////////////////////////////////
-/*
-*user chooses vsp or vscomp
-*class of either one has made
-
-*game
-*vsplayer [ inherits game]
-*vscomp [ inherits game]
-
-*game - var: winner condition, btns
-        functions: check winner, draw, reset, start, btn-textupdate and disable
-
-*vsplayer - vars: [palyer1 = x , player2 = o] 
-            functions: 
-
-*vscomp - vars: [palyer1 = x , computer = o] 
-          functions: ispositionfree, compmove, minimax
-  */    
-////////////////////////////////////////////////////////////////////////////////////////
-
-class  Game{
-constructor(){
+const gameContainer = document.querySelector(".game-container");
+const playerVscompModel = document.getElementById("vsplayerOrvscomp");
+let onGame = null;
+class Game {
+  constructor() {
+    this.start = false;
     this.winnerCondition = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ]
-  
-  this.btns = document.querySelectorAll(".btn")
-}
-checkDraw(){
-  // //check all btns are filled
-  // let emptyBtnTrack= false
-  // this.btns.forEach(btn=>{
-     
-  //   if(btn.innerText===""){
-  //    emptyBtnTrack = true
-  //   }
-  // })
-  // if(emptyBtnTrack){
-  //   return
-  // }
-  // else
-  // alert("draw")
-  // ..since in foreach using return only returns from loop not the function so alert is calledd again and again better to use for of loop 
-  for(let btn of this.btns){
-   if(btn.innerText==="") return
-  }
-  alert("draw")
-}
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
 
-  checkWinner(){
-    for(let pattern of this.winnerCondition){
-      console.log(pattern)
-      // make 3 variables who will have inner text of the btns in this pattern
-      let btn1 = this.btns[pattern[0]].innerText
-      let btn2 = this.btns[pattern[1]].innerText
-      let btn3 = this.btns[pattern[2]].innerText
-      console.log(btn1,btn2,btn3);
-      
-      if(btn1 === btn2 && btn2 === btn3 && btn1!=""){
-        alert(btn1+ " won")
-        this.resetBoard()
-        return 
+    this.btns = document.querySelectorAll(".btn");
+  }
+
+  checkDraw() {
+    for (let btn of this.btns) {
+      if (btn.innerText === "") return;
+    }
+    this.showResult("Draw!");
+  }
+
+  checkWinner() {
+    for (let pattern of this.winnerCondition) {
+      let btn1 = this.btns[pattern[0]].innerText;
+      let btn2 = this.btns[pattern[1]].innerText;
+      let btn3 = this.btns[pattern[2]].innerText;
+      if (btn1 === btn2 && btn2 === btn3 && btn1 != "") {
+        this.showResult(btn1 + " won");
+        return true;
       }
-
     }
-    //if no one won check if draw
-    this.checkDraw()
-  }
-  resetBoard(){
-
-      this.btns.forEach(btn => {
-      btn.innerText=""
-      btn.disabled=false
-  
-})
+    this.checkDraw();
   }
 
-  updateMove(btn,turn){
-    if(btn.innerText===""){
-      btn.innerText= turn
-      btn.disabled = true
-    }
-    else{
-      alert("box is not empty")
-    }
-  }
-}
-
-// let game = new Game()
-// console.log(game.winnerCondition)
-// console.log(game.btns)
-// game.checkWinner()
-
-class vsplayer extends Game{
-  constructor(){
-    super()
-    this.start= false
-    this.player1 = "O"
-    this.player2 = "X"
-    this.turn = this.player1
-  }
-
-  startGame(){
-    console.log("player game staterd");
-    
-    this.btns.forEach(btn => {
-       btn.addEventListener("click",()=>{
-        if(!this.start){
-          this.start = true
-          console.log("start turn: ",this.turn);
-          this.updateMove(btn,this.turn)
-          this.turn = this.turn===this.player1?this.player2:this.player1;
-        }
-        else{
-          console.log("rest game turn: ",this.turn);
-          this.updateMove(btn,this.turn)
-          this.turn = this.turn===this.player1?this.player2:this.player1;
-          this.checkWinner()
-        }
-       })
+  resetBoard() {
+    this.btns.forEach((btn) => {
+      btn.innerText = "";
+      btn.disabled = false;
     });
   }
+
+  showResult(message) {
+    let resultContainer = document.getElementById("result");
+    resultContainer.classList.remove("hide");
+    gameContainer.classList.add("blur");
+    document.getElementById("resultMessage").innerText = message;
+  }
+
+  updateMove(btn, turn) {
+    if (btn.innerText === "") {
+      btn.innerText = turn;
+      btn.disabled = true;
+    } else {
+      alert("box is not empty");
+    }
+  }
 }
 
-document.getElementById("vsPlayer").addEventListener("click",()=>{
-  console.log("players moves");
-  let playerGame = new vsplayer()
-  playerGame.startGame()
-  document.getElementById("restart").addEventListener("click",()=>{
-    playerGame.resetBoard()
-  })
-})
+class vsplayer extends Game {
+  constructor() {
+    super();
+    this.player1 = "O";
+    this.player2 = "X";
+    this.turn = this.player1;
+  }
+
+  togglePlayer() {
+    this.turn = this.turn === this.player1 ? this.player2 : this.player1;
+  }
+
+  addEventsToBtns() {
+    this.btns.forEach((btn) => {
+      btn.onclick = () => {
+        if (!this.start) {
+          this.start = true;
+          this.updateMove(btn, this.turn);
+          this.togglePlayer();
+        } else {
+          this.updateMove(btn, this.turn);
+          this.togglePlayer();
+          this.checkWinner();
+        }
+      };
+    });
+  }
+  startGame() {
+    this.addEventsToBtns();
+  }
+}
+
+function showPlayerVsCompModel() {
+  playerVscompModel.classList.remove("hide");
+  gameContainer.classList.add("blur");
+}
+function hidePlayerVsCompModel() {
+  playerVscompModel.classList.add("hide");
+  gameContainer.classList.remove("blur");
+}
+
+document.getElementById("restart").addEventListener("click", () => {
+  onGame.resetBoard();
+});
+
+document.getElementById("start").addEventListener("click", () => {
+  gameContainer.classList.remove("blur");
+  document.getElementById("result").classList.add("hide");
+  showPlayerVsCompModel();
+  onGame.resetBoard();
+  onGame = null;
+});
+
+document.getElementById("vsPlayer").addEventListener("click", () => {
+  console.log("player game staterd");
+  hidePlayerVsCompModel();
+  onGame = new vsplayer();
+  onGame.startGame();
+});
+
+class vsComp extends Game {
+  constructor() {
+    super();
+    this.player = "X";
+    this.bot = "O";
+  }
+  board() {
+    return Array.from(this.btns).map((btn) => btn.innerText);
+  }
+  startGame() {
+    this.btns.forEach((btn) => {
+      btn.onclick = () => {
+        if (!this.start) {
+          this.start = true;
+          console.log("in startgame if block");
+          this.updateMove(btn, this.player);
+          let boared = this.board();
+          console.log(boared);
+          this.computeMove(boared);
+        } else {
+          this.updateMove(btn, this.player);
+          console.log("in startgame else block");
+          let boared = this.board();
+          console.log(boared + " inside startame");
+          this.checkWinner();
+          this.computeMove(boared);
+          this.checkWinner();
+        }
+      };
+    });
+  }
+  checkIfWon(board) {
+    if (board[0] === board[1] && board[1] === board[2] && board[0] !== "")
+      return board[0];
+    else if (board[3] === board[4] && board[4] === board[5] && board[3] !== "")
+      return board[3];
+    else if (board[6] === board[7] && board[7] === board[8] && board[6] !== "")
+      return board[6];
+    else if (board[0] === board[3] && board[3] === board[6] && board[0] !== "")
+      return board[0];
+    else if (board[1] === board[4] && board[4] === board[7] && board[1] !== "")
+      return board[1];
+    else if (board[2] === board[5] && board[5] === board[8] && board[2] !== "")
+      return board[2];
+    else if (board[2] === board[4] && board[4] === board[6] && board[2] !== "")
+      return board[2];
+    else if (board[0] === board[4] && board[4] === board[8] && board[0] !== "")
+      return board[0];
+    else return undefined;
+  }
+
+  checkIfDraw(board) {
+    for (let btn of board) {
+      if (btn === "") return false;
+    }
+    return true;
+  }
+  computeMove(board) {
+    let bestScore = -800;
+    let bestMove = 0,
+      score = 0;
+    for (let key = 0; key < board.length; key++) {
+      if (board[key] === "") {
+        board[key] = this.bot;
+        score = this.minmax(board, 0, false);
+        board[key] = "";
+        if (score > bestScore) {
+          bestScore = score;
+          bestMove = key;
+        }
+      }
+    }
+    if (bestMove) {
+      this.updateMove(this.btns[bestMove], this.bot);
+    }
+    return;
+  }
+
+  minmax(board, depth, isMaximizing) {
+    let winMark = this.checkIfWon(board);
+    if (winMark === this.player) return -1;
+    else if (winMark === this.bot) return 1;
+    else if (this.checkIfDraw(board)) return 0;
+
+    if (isMaximizing) {
+      let bestScore = -800;
+      let score = null;
+      for (let key in board) {
+        if (board[key] === "") {
+          board[key] = this.bot;
+          score = this.minmax(board, depth + 1, false);
+          board[key] = "";
+          if (score > bestScore) {
+            bestScore = score;
+          }
+        }
+      }
+      return bestScore;
+    } else {
+      let bestScore = 800;
+      let score = null;
+      for (let key in board) {
+        if (board[key] === "") {
+          board[key] = this.player;
+          score = this.minmax(board, depth + 1, true);
+          board[key] = "";
+          if (score < bestScore) {
+            bestScore = score;
+          }
+        }
+      }
+      return bestScore;
+    }
+  }
+}
+
+document.getElementById("vsComputer").addEventListener("click", () => {
+  console.log("computer game started");
+  hidePlayerVsCompModel();
+  onGame = new vsComp();
+  onGame.startGame();
+});
